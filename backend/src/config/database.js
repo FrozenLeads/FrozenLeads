53a420bb7ce2f6b13d4f3c7ea693604bd1d4b854
FrozenLeads <leadsfrozen@gmail.com>
2025-06-13 01:42:07 +0530

    Revert "A"

--- backend/src/config/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const ConnectDb = async ()=>{
-    await mongoose.connect('mongodb://localhost:27017/FrozenLeads', {autoIndex: true})
-}
-module.exports = ConnectDb;
